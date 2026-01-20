b701c53f8022b5a9831e8080fadaf3c966d5244e
Jake Hamilton <jh0695@gmail.com>
2026-01-20 14:36:53 -0500

    Initial commit: Created .NET MAUI FitTrack project

---
+++ FitTrack/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FitTrack
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
